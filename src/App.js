c510352e1e7a81e9739b65ccda8b41ce78772b51
Alamin-eng <alaminruben@gmail.com>
2023-11-01 21:39:28 +0000

    second

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect} from 'react';
 import './App.css';
-import axios from "axios";
+// import axios from "axios";
 import Header from './Header';
 import Main from './Main';
 
