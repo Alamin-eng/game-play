ce7a360f9aae2b37411a9202347ad48d653ba211
Alamin-eng <alaminruben@gmail.com>
2023-12-18 16:08:07 +0000

    resp font size fixed

--- src/popular-platforms.js
+++ src/popular-platforms.js
@@ -8,69 +8,75 @@ export default function PopularPlatforms() {
   return (
     <div className="popular-platforms">
       <a
-        className=""
         style={{
           textDecoration: "none",
-          fontSize: "calc(1.1rem +.5rem)",
-          color: "#0a9800",
+          fontSize: "1rem",
         }}
         href="https://www.xbox.com/en-gb/games?xr=shellnav"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaXbox style={{ fontSize: "17px" }} />
+        <FaXbox
+          style={{
+            color: "rgb(0, 192, 33)",
+          }}
+        />
       </a>
       <a
         className=""
         style={{
           textDecoration: "none",
-          fontSize: "calc(1.1rem +.5rem)",
-          color: "white",
+          fontSize: "1rem",
+          color: "rgb(210, 245, 255)",
         }}
         href="https://store.steampowered.com/pccafe/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaSteamSquare style={{ fontSize: "17px" }} />
-      </a>
-      <a
-        className=""
-        style={{
-          textDecoration: "none",
-          fontSize: "calc(1.1rem +.5rem)",
-          color: "#0029f3",
-        }}
-        href="https://www.playstation.com/en-gb/games/pc-games/?smcid=pdc%3Aen-gb%3Aps4-ps4-games%3Aprimary%20nav%3Amsg-games%3Apc"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <BsPlaystation style={{ fontSize: "19px" }} />
-      </a>
-      <a
-        className=""
-        style={{
-          textDecoration: "none",
-          fontSize: "calc(1.1rem +.5rem)",
-          color: "white",
-        }}
-        href="https://store.epicgames.com/en-US/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <SiEpicgames style={{ fontSize: "17px" }} />
+        <FaSteamSquare />
       </a>
       <a
         className=""
         style={{
           textDecoration: "none",
           fontSize: "1.1rem",
-          color: "#cd5c00",
+        }}
+        href="https://www.playstation.com/en-gb/games/pc-games/?smcid=pdc%3Aen-gb%3Aps4-ps4-games%3Aprimary%20nav%3Amsg-games%3Apc"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsPlaystation
+          style={{
+            color:
+              "linear-gradient(300deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 41%, rgba(0,212,255,1) 100%)",
+          }}
+        />
+      </a>
+      <a
+        className=""
+        style={{
+          textDecoration: "none",
+          fontSize: "1rem",
+          color: "white",
+        }}
+        href="https://store.epicgames.com/en-US/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <SiEpicgames SiEpicgames />
+      </a>
+      <a
+        className=""
+        style={{
+          textDecoration: "none",
+          fontSize: "1.1rem",
+          color: "#c93500",
         }}
         href="https://store.steampowered.com/vr"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <BsBadgeVrFill style={{ fontSize: "18px" }} />
+        <BsBadgeVrFill />
       </a>
     </div>
   );
